88da68ba169f97289cdc43618b7b92b0a5297ee5
takahiro <nobooknolife16@gmail.com>
2018-05-30 18:49:38 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+ 25.times do |n|
+ 	Diction.create(name: "金融#{n}", category_parent: "金融", category: "金融", user_id: 1, public_flg: true)
+ end
+
+ 25.times do |n|
+ 	Diction.create(name: "建設・不動産#{n}", category_parent: "建設・不動産", category: "建設", user_id: 1, public_flg: true)
+ end
+
+ 25.times do |n|
+ 	Contact.create(title: "お問い合わせ#{n}", content: "お問い合わせ#{n}", user_id: 1)
+ end
+
