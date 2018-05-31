ec542b665428ebabd644ea43cd0e3e797458779f
takahiro <nobooknolife16@gmail.com>
2018-05-31 15:35:47 +0900

    devise error => bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,15 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Don't care if the mailer can't send.　#メールアドレスの設定
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true #falseがデフォルト
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :port => 587,
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 
   config.action_mailer.perform_caching = false
 
